73c43aaff5ebe83a1b68f798490057c2bf77a4c5
Mine <Mine@DESKTOP-Q52F920>
2022-12-10 21:39:51 +0900

    2022-12-10
    스프링 부트 프로젝트 업로드

---
+++ toyproject_01/src/main/java/com/example/demo/Toyproject01Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Toyproject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Toyproject01Application.class, args);
+	}
+
+}
